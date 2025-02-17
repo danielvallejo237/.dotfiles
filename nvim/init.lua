4dc443813a9379e822fe1203e97feee249c827c9
danielvallejo237 <40125758+danielvallejo237@users.noreply.github.com>
2025-02-17 10:05:22 -0600

    Add files via upload
    Actualización del archivo de init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,6 @@
-
-require'nvim-treesitter.install'.compilers = { 'gcc' }
--- Save and copy
-require("nvim-treesitter.configs").setup({
-  -- ... other configurations ...
-  highlight = {
-    enable = true, -- Enable Treesitter highlighting
-  }
-})
-vim.cmd('colorscheme base16-equilibrium-dark') -- Set colourscheme
+-- In your init.lua file
+local opts = { noremap = true, silent = true }
+-- Trigger completion with <C-Space>
 vim.cmd('highlight Comment cterm=bold') -- Italicize comments
 vim.cmd('syntax on') -- Enable syntax highlighting
 require("dt.base") -- General Settings
@@ -16,3 +9,14 @@ require("dt.maps") -- Keymaps
 require("dt.plugins") -- Plugins
 require("dt.bootstrap") -- Packer Auto-Installer
 require("dt.telescope") -- Packer Auto-Installer
+require("dt.ufo") -- Packer Auto-Installer
+require("dt.gruvbox") -- Packer Auto-Installer
+vim.cmd('colorscheme gruvbox') -- Set colourscheme
+require'nvim-treesitter.install'.compilers = { 'gcc' }
+-- Save and copy
+require("nvim-treesitter.configs").setup({
+  -- ... other configurations ...
+  highlight = {
+    enable = true, -- Enable Treesitter highlighting
+  }
+})
